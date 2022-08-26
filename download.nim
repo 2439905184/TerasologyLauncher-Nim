bdd13cff99523cbfce025b917e1bfd8376b49032
KangKang <44329867+Cjjj-sys@users.noreply.github.com>
2022-08-26 10:54:14 +0800

    🦄 refactor: 重新命名为下划线形式

--- download.nim
+++ download.nim
@@ -7,10 +7,10 @@ import zippy/ziparchives
 type
   DownloadInfo* = object # 存储下载信息
     content_type*, download_url*: string
-    size: int
+    size*: int
   ReleaseData* = object # 存储发布的数据
     tag_name*, name*, published_at*: string
-    downloadInfos: seq[DownloadInfo]
+    downloadInfos*: seq[DownloadInfo]
 
 proc download_jre*(version:string) = 
   
@@ -22,28 +22,28 @@ proc resolve_jre*(version:string) =
   extractAll(zip, "jre/" & version)
   echo "安装完成"
 
-proc asyncGet(url: string): Future[string] {.async.} =
+proc async_get(url: string): Future[string] {.async.} =
   var client = newAsyncHttpClient()
   return await client.getContent(url)
 
-proc getReleaseDatas(): seq[ReleaseData] =
-  var jsonRaw: string = waitFor asyncGet("https://api.github.com/repos/MovingBlocks/Terasology/releases")
-  var parsedJson = parseJson(jsonRaw)
-  var releaseDatas: seq[ReleaseData]
-  for releaseItem in parsedJson:
+proc get_release_datas*(): seq[ReleaseData] =
+  var json_raw: string = waitFor async_get("https://api.github.com/repos/MovingBlocks/Terasology/releases")
+  var parsed_json = parseJson(json_raw)
+  var release_datas: seq[ReleaseData]
+  for release_item in parsed_json:
     var 
-      assetsNode: JsonNode
-      downloadInfos: seq[DownloadInfo]
-    assetsNode = releaseItem["assets"]
-    for assetItem in assetsNode:
-      downloadInfos.add(DownloadInfo(content_type : assetItem["content_type"].getStr(), 
-                                    download_url : assetItem["browser_download_url"].getStr(), 
-                                    size : assetItem["size"].getInt()))
-    releaseDatas.add(ReleaseData(tag_name : releaseItem["tag_name"].getStr(), 
-                                name : releaseItem["name"].getStr(), 
-                                published_at : releaseItem["published_at"].getStr(),
-                                downloadInfos : downloadInfos))
-  return releaseDatas
+      assets_node: JsonNode
+      download_infos: seq[DownloadInfo]
+    assets_node = release_item["assets"]
+    for asset_item in assets_node:
+      download_infos.add(DownloadInfo(content_type : asset_item["content_type"].getStr(), 
+                                    download_url : asset_item["browser_download_url"].getStr(), 
+                                    size : asset_item["size"].getInt()))
+    release_datas.add(ReleaseData(tag_name : release_item["tag_name"].getStr(), 
+                                name : release_item["name"].getStr(), 
+                                published_at : release_item["published_at"].getStr(),
+                                downloadInfos : download_infos))
+  return release_datas
   
 # 此处使用github加速代理服务 如果下载失败，请打开浏览器手动下载, https://ghproxy.com/
 
