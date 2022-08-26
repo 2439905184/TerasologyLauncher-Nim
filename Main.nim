d4c96055d19b847218f164b16fb9afed5bde9503
2439905184 <suchenglong018@126.com>
2022-08-26 10:28:01 +0800

    ✨ feat: 初步实现解压安装jre

--- Main.nim
+++ Main.nim
@@ -1,48 +1,12 @@
 import std/os
 #import wNim
-import download
-
-# let app = App()
-# let frame = Frame(title="Launcher", size=(800,600))
-# let panel = Panel(frame)
-
-# proc on_run_pressed() = 
-#   MessageDialog(frame, "Hello World", "MessageDialog").display()
-
-# proc on_download_pressed() = 
-#   #download_jre(frame)
-#   #let ReleaseCheckBox = CheckBox(frame, label="正式版")
-#   #let TestCheckBox = CheckBox(frame, lable="测试版")
-#   #style=wLcSingleSel
-#   let FilterHBox = ListCtrl(frame, pos=(50,0))
-#   FilterHBox.init(frame)
-#   FilterHBox.appendItem("正式版")
-#   FilterHBox.appendItem("测试版")
-#   #ReleaseCheckBox.position = ()
-#   #TestCheckBox.position = ()
-
-# proc layout() = 
-#   let ButtonGame = Button(panel, label="下载")
-#   let ButtonRun = Button(panel, label="启动游戏")
-#   ButtonRun.position = (0, 100)
-#   ButtonGame.connect(wEvent_LeftDown, on_download_pressed)
-#   ButtonRun.connect(wEvent_LeftDown, on_run_pressed)
-# layout()
-
-
-# frame.center()
-# frame.show()
-# app.mainLoop()
+#import download
+import zippy/ziparchives
 
 proc setup_version(version:string) = 
   var path = "games/" & version
 
-proc unzip() = 
-
-  discard
-
 proc run() = 
-
   discard execShellCmd("java.exe -jar D:/work/myTerasologyLauncher/dist/games/libs/Terasology.jar")
 
 var params = commandLineParams()
@@ -50,5 +14,12 @@ var params = commandLineParams()
 if params[0] == "download":
   var version = params[1]
   setup_version(version)
-  download_game(version)
-  discard
+  #download_game(version)
+
+if params[0] == "install_jre":
+  var version = params[1]
+  #download_jre(version)
+  #resolve_jre(version)
+  echo "解压中..."
+  extractAll("download/jre.zip", "jre/11")
+  echo "安装完成"
