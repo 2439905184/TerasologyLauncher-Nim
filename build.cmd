acabbbe975975e351e39a3f52875d8c11a4d6da2
2439905184 <suchenglong018@126.com>
2022-08-26 11:59:09 +0800

    🐳 chore: 添加zippy依赖

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-nim c -d:ssl --outdir:dist Main.nim
+nim c -d:ssl -d:zippy --outdir:dist Main.nim
 rem nim c -d:ssl -d:release --outdir:dist download.nim
 rem nim c -d:release --outdir:dist nvidia.nim
