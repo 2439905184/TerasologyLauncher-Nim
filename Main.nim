1fa8b3d2416936c43cac7b3b8ae7d0b73fb2d65c
2439905184 <suchenglong018@126.com>
2022-08-26 16:52:08 +0800

    🎈 perf: 使用相对路径启动游戏

--- Main.nim
+++ Main.nim
@@ -3,7 +3,6 @@ import std/os
 import std/asyncdispatch
 import download
 
-
 var params = commandLineParams()
 
 if params[0] == "init":
@@ -19,6 +18,7 @@ if params[0] == "download_game":
 if params[0] == "install_game":
   var version = params[1]
   install_game(version)
+
 if params[0] == "download_jre":
   var version = params[1]
   download_jre(version)
@@ -30,6 +30,7 @@ if params[0] == "install_jre":
 if params[0] == "run":
   var version = params[1]
   setCurrentDir("jre/11/jre-11.0.16.1/bin")
-  var success = execShellCmd("java.exe -jar D:/work/myTerasologyLauncher/dist/games/" & version & "/libs/Terasology.jar")
+  # var success = execShellCmd("java.exe -jar D:/work/myTerasologyLauncher/dist/games/" & version & "/libs/Terasology.jar")
+  var success = execShellCmd("java.exe -jar " & "../../../../games/" & version & "/libs/Terasology.jar")
   if success == 0:
     echo "游戏启动失败或者进程已结束！"
