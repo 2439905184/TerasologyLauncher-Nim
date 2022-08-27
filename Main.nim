94d7481cf9ed9bf175dae08f00ef9931ddf7e807
2439905184 <suchenglong018@126.com>
2022-08-27 15:34:46 +0800

    🌈 style: 美化help输出

--- Main.nim
+++ Main.nim
@@ -10,15 +10,27 @@ if len(params) == 0:
 if params[0] == "help":
   echo "欢迎使用本启动器 下面是使用说明"
   echo "可以使用的参数列表 \n"
-  echo "init: 初始化启动器，创建文件夹 \n"
-  echo "download_game: 下载游戏"
-  echo "例子: download_game v5.2.0 \n"
-  echo "install_game: 安装游戏"
-  echo "例子: install_game v5.2.0 \n"
+
+  echo "init: 初始化启动器"
+  echo "例子: init \n"
+
+  echo "change_proxy: 更改在线代理"
+  echo "例子: change_proxy fastgithub \n"
+  
   echo "download_jre: 下载java"
   echo "例子: download_jre 8 \n"
-  echo "install_jre: 安装jre"
-  echo "例子: install_jre 8"
+
+  echo "install_jre: 安装java"
+  echo "例子: install_jre 8 \n"
+
+  echo "download_game: 下载游戏" 
+  echo "例子: download_game v3.0.0 \n"
+
+  echo "install_game: 安装游戏"
+  echo "例子: install_game v3.0.0 \n"
+
+  echo "help: 获取帮助"
+  echo "例子: help"
 
 if params[0] == "init":
   createDir("download")
