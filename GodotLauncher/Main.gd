c0ec718afe3719fd169243a05d2c7d4da1c6be14
2439905184 <suchenglong018@126.com>
2022-08-31 19:56:29 +0800

    ✨ feat: 添加切换版本功能

--- GodotLauncher/Main.gd
+++ GodotLauncher/Main.gd
@@ -11,8 +11,7 @@ func _ready():
 	
 func _on_Run_pressed():
 	var exit_code = OS.execute("Main.exe",["run",select_version],false)
-	if exit_code != OK:
-		print_debug("出错了")
+	#if exit_code != OK: print_debug("出错了")
 	pass
 	
 # 执行下载
@@ -50,3 +49,49 @@ func _on_change_proxy(p_proxy):
 	elif exit == OK: OS.alert("已切换代理")
 	pass
 
+
+
+func _on_ListGame_pressed():
+	var output = []
+	var exit_code = OS.execute("Main.exe",["list_installed"],true,output)
+	if exit_code != OK: OS.alert("出错了")
+	OS.shell_open("games")
+#	elif exit_code == OK:
+#		for v in output:
+#			if v == "v3.2.0": $InstalledPanel/VersionList/v1/Label.text = "已安装"
+#			if v == "v2.0.0": 
+#				print_debug(v,"已安装")
+#				$InstalledPanel/VersionList/v2/Label.text = "已安装"
+#			if v == "v1.6.0": $InstalledPanel/VersionList/v3/Label.text = "已安装"
+#			if v == "v1.3.0": $InstalledPanel/VersionList/v4/Label.text = "已安装"
+#			print_debug(v)
+#		$InstalledPanel.show()
+#
+#	print_debug(output)
+	
+	pass
+
+func _on_GameList_pressed():
+	var GameList = $GameList
+	var path = OS.get_executable_path().get_base_dir() + "/games"
+	print_debug(path)
+	var dir = Directory.new()
+	var dirs = []
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print_debug("发现目录：" + file_name)
+				file_name = dir.get_next()
+				dirs.append(file_name)
+	else:
+		print_debug("尝试访问路径时出错。")
+	GameList.clear()
+	for i in dirs:
+		GameList.add_item(i)
+	pass
+
+func _on_GameList_item_selected(index):
+	select_version = $GameList.get_item_text(index)
+	pass
