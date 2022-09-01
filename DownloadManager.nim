5ec014c2ec6720f5a710794e8f4c861f42b9e0a9
2439905184 <suchenglong018@126.com>
2022-09-01 09:36:46 +0800

    🎈 perf: 跟换jre下载地址
    nyan盘

--- DownloadManager.nim
+++ DownloadManager.nim
@@ -8,6 +8,7 @@ const github_release = "https://github.com/MovingBlocks/Terasology/releases/down
 var download_version = ""
 var proxy_fastgithub = "https://hub.fastgit.xyz/"
 var proxy_ghproxy* = "https://ghproxy.com/"
+const lanzou_api1 = "https://www.youwk.cn/api/lanzou"
 var select_proxy = proxy_ghproxy #用户选择的代理 默认为ghproxy
 
 type
@@ -31,19 +32,50 @@ proc download_jre*(version:string) =
     var content = client.getContent("https://download.bell-sw.com/java/11.0.16.1+1/bellsoft-jre11.0.16.1+1-windows-amd64.zip")
     writefile("download/jre11.zip", content)
     echo "下载完成"
-  if version == "8":
+  if version == "8": 
     var url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/372924428/5c3e51b9-d2fb-4baf-9cf4-c6de7698a210?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220827%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220827T042433Z&X-Amz-Expires=300&X-Amz-Signature=d61468e10cd20d0b452f4f2b33f40ae533b849ec9b4b5b2a2e9344a59050b284&X-Amz-SignedHeaders=host&actor_id=29478722&key_id=0&repo_id=372924428&response-content-disposition=attachment%3B%20filename%3DOpenJDK8U-jre_x64_windows_hotspot_8u345b01.zip&response-content-type=application%2Foctet-stream"
     var url2 = "https://download.bell-sw.com/java/8u345+1/bellsoft-jre8u345+1-windows-amd64.zip"
     var oracle_java8 = "https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u341-b10/424b9da4b48848379167015dcc250d8d/jre-8u341-windows-x64.exe?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u341-b10/424b9da4b48848379167015dcc250d8d/jre-8u341-windows-x64.exe&BHost=javadl.sun.com&File=jre-8u341-windows-x64.exe"
+    const origin_lanzou = "https://wwp.lanzouy.com/i2Tl90ar0d5g"
     if fileExists("download/jre8.exe"): echo "文件已存在"
     else:
       echo "下载网站: " & oracle_java8 
       echo "开始下载jre8 请稍等..."
       var client = newHttpClient()
       client.onProgressChanged = onProgressChanged
-      var content = client.getContent(oracle_java8)
-      writefile("download/jre8.exe", content)
-      echo "下载完成"
+      try:
+        var content = client.getContent(oracle_java8)
+        writefile("download/jre8.exe", content)
+        echo "下载完成"
+      except HttpRequestError:
+        echo "尝试从oracle下载jre失败！"
+        echo "你可以前往oracle手动下载"
+        echo "https://www.java.com/zh-CN/"
+        echo "切换到nya盘 下载中 请稍等..."
+        var c = newHttpClient()
+        var u = "https://pan.nyaku.moe/api/v3/file/source/5616/jre-8u341-windows-x64.exe?sign=a7oXRML6TZ_H7jAjoC81pUJN-dFASWW633Sq9F-kVQU%3D%3A0"
+        var uu = "https://things.nyapan.mouup.top/personal/schuyi_iamswlx_onmicrosoft_com/_layouts/15/download.aspx?UniqueId=f57195ca-603d-4267-b40a-91532584bd72&Translate=false&tempauth=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvaWFtc3dseC1teS5zaGFyZXBvaW50LmNvbUAyYmE5YzYwOC1mMzEwLTRjOTItODNmOC1kMjY2MzZmMDY2MmYiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNjYxOTk1NDY2IiwiZXhwIjoiMTY2MTk5OTA2NiIsImVuZHBvaW50dXJsIjoiNzFudzhITS9PZDIwRloxdlRwY0RRVnNnOUVJUTNsdCtXMTdQRStQSUtybz0iLCJlbmRwb2ludHVybExlbmd0aCI6IjE2MSIsImlzbG9vcGJhY2siOiJUcnVlIiwiY2lkIjoiTXpKbU56a3dZekV0TURBMlpTMDBaVE00TFdJNU5HVXRPREkwTm1ZeFlUTXhaVGs0IiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik9UaG1aalV3T1dRdE9USTNaQzAwWm1JM0xUZzRNR0l0WXpZeVkyVmlOVGs0TkRsbSIsImFwcF9kaXNwbGF5bmFtZSI6Im55YXBhbiIsImdpdmVuX25hbWUiOiLpm6rkuIAiLCJmYW1pbHlfbmFtZSI6IuS4gOS5i-a_kSIsImFwcGlkIjoiYWU1NzQyNTMtY2I3ZC00OTAxLWIxZGItMDc1NmQyOGE1ZTVjIiwidGlkIjoiMmJhOWM2MDgtZjMxMC00YzkyLTgzZjgtZDI2NjM2ZjA2NjJmIiwidXBuIjoic2NodXlpQGlhbXN3bHgub25taWNyb3NvZnQuY29tIiwicHVpZCI6IjEwMDMyMDAxQzdFNTE3NjUiLCJjYWNoZWtleSI6IjBoLmZ8bWVtYmVyc2hpcHwxMDAzMjAwMWM3ZTUxNzY1QGxpdmUuY29tIiwic2NwIjoiYWxsZmlsZXMud3JpdGUiLCJ0dCI6IjIiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjpudWxsLCJpcGFkZHIiOiIyMC4xOTAuMTQ0LjE3MSJ9.RlEvdjI5Z1lXQmtxR1ZYQUg3M3V4eGorY0NHdEtscHhyeTYrRjR6VzNFZz0&ApiVersion=2.0"
+        var d = c.getContent(uu)
+        c.onProgressChanged = onProgressChanged
+        writefile("download/jre8.exe",d)
+        echo "下载完成"
+        # 需要签名校验
+        # echo "切换到蓝奏云解析"
+        # var lanzou_java8 = lanzou_api1 & "?key=leJlQOEoNCkIgKbSOU8UUTfDg1xjFD&url=https://wwp.lanzouy.com/i2Tl90ar0d5g"
+        # var content = client.getContent(lanzou_java8)
+        # var parsed_json = parseJson(content)
+        # if parsed_json["msg"].getStr() == "请求成功":
+        #   var d_url = parsed_json["data"]["dom"].getStr()
+        #   echo "下载地址"
+        #   echo d_url
+        #   var client_lanzou = newHttpClient()
+        #   client_lanzou.onProgressChanged = onProgressChanged
+        #   var content_jre = client_lanzou.getContent(d_url)
+        #   writefile("download/jre.exe",content_jre)
+        #   echo "下载完成"
+        # else:
+        #   echo "蓝奏云解析失败 请手动下载"
+        #   echo origin_lanzou
 
 proc install_jre*(version:string) = 
   # 用于安装其他版本的保留代码
